e62c0bf2bdf591eabca0f8694f83b298c1b519a4
Edd Crossman <eddcrossman@hotmail.com>
2023-04-12 16:08:14 +0100

    feat: added locations list

--- js/app.js
+++ js/app.js
@@ -12,9 +12,48 @@ const initialMap = {
     }
 };
 
+const initialLocations = [
+    {
+        name: 'Mumbai',
+        lat: 19,
+        long: 73
+    },
+    {
+        name: 'Hampi',
+        lat: 15.333355390258228,
+        long: 76.4633346977881
+    },
+    {
+        name: 'Madurai',
+        lat: 9.92459880544921,
+        long: 78.12110650578332
+    },
+    {
+        name: 'Kanyakumari',
+        lat: 8.078541501887765,
+        long: 77.5553547315407
+    },
+    {
+        name: 'Varkala',
+        lat: 8.738166316264463, 
+        long: 76.71739663566046,
+    }
+]
+
 const ViewModel = function() {
     var self = this;
     this.currentMap = ko.observable(new Map(initialMap));
+    this.locationList = ko.observableArray([]);
+
+    initialLocations.forEach(function(locationItem) {
+        self.locationList.push( new Location(locationItem));
+    });
+}
+
+const Location = function(data) {
+    this.name = ko.observable(data.name);
+    this.lat = ko.observable(data.lat);
+    this.long = ko.observable(data.long);
 }
 
 const Map = function(data) {
@@ -31,6 +70,7 @@ const Map = function(data) {
             url += `&${i}=${data.params[i]}`;
         }
 
+        url += `&markers=color:blue%7Csize:mid%7Clabel:T%7C19,73`
         url += `&key=${API_KEY}`
 
         return url;
